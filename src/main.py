e9b288ff4d752e8a773eda0bd59451a7a1661083
KEHUI YAO <ykhray19960527@gmail.com>
2021-07-20 10:27:08 +0800

    reduce batch size

--- src/main.py
+++ src/main.py
@@ -592,7 +592,7 @@ if __name__ == "__main__":
     parser.add_argument('--dropout_rate', type=float, default=0, help='dropout rate for all layers')
     parser.add_argument('--training_data_size', type=int, default=80)
     parser.add_argument('--validation_data_size', type=int, default=20)
-    parser.add_argument('--batch_size', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--gpu', type=int, default=1, help='type 1 if you want to use gpu, type 0 if you want to use cpu')
 
 
